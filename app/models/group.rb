35f16eed564530e00449ae99a322f112f427fea4
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-10-11 15:37:58 +0900

    make_make

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,16 @@ class Group < ApplicationRecord
   has_many :users ,through: :group_users
   has_many :group_users
   has_many :messages
-  validates :name, presence: true, uniqueness: true
+
+  validates :name, presence: true
+
+
+  def sow_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'nononon'
+    end
+  end
 
 end
