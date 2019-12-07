5da818a86780b4a825a038ed9831977d36aadda3
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-12-08 02:05:01 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
 
   root 'groups#index'
 
-  resources  :users, only: [:edit,:index,:update]
+  resources  :users, only: [:edit,:index,:update,:show]
   resources  :groups, only: [:create,:edit,:update,:new] do
     resources :messages, only: [:index,:create]
 
     namespace :api do
       resources :messages, only: :index, defaults: { format:'json' }
     end
-    
+
   end
 
 end
